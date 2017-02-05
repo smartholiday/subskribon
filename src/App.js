7a4bf07258ebde4338b843ae9c66e1fbf5f7f8d8
Mehdi Ben Abdallah <mehdi.se.insat@gmail.com>
2017-02-05 18:47:32 +0100

    initial state: title -> civility

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      title: '',
+      civility: '',
       full_name: '',
       occupation: '',
       phone: '',
